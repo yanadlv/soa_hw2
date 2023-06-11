09d0437ea05eb5bb0b5816c88da8f1241f4cd808
yana <74400071+yanadlv@users.noreply.github.com>
2023-06-12 02:47:04 +0300

    Update client.py

--- client.py
+++ client.py
@@ -30,35 +30,27 @@ async def subscribe_to_notifications(stub):
     request = server_pb2.SubscribeToNotificationsRequest()
     responses = stub.SubscribeToNotifications(request)
     async for response in responses:
-        print(f"> NOTIFICATION: {response.message}", flush=True)
+        print(f"> NOTIFICATION: {response.message}")
 
 
 async def commands_loop(stub):
-    is_subscribed = False
-    subscription_task = None
+    task = asyncio.create_task(subscribe_to_notifications(stub))
+    await asyncio.sleep(0.1)
     while True:
         request = input()
         if request == "PLAYERS":
             await get_connected_players(stub)
-            print("\nReady for the next command: QUIT / PLAYERS" +
-                  (" / SUBSCRIBE" if not is_subscribed else "") + "\n")
-        elif request == "SUBSCRIBE":
-            is_subscribed = True
-            print("\nYou are now subscribed to notifications")
-            print("\nReady for the next command: QUIT / PLAYERS\n")
-            if not subscription_task or subscription_task.done():
-                subscription_task = asyncio.ensure_future(subscribe_to_notifications(stub))
+            print("\nReady for the next command: PLAYERS / REFRESH / QUIT\n")
+        elif request == "REFRESH":
+            await asyncio.sleep(0.1)
+            print("\nReady for the next command: PLAYERS / REFRESH / QUIT\n")
         elif request == "QUIT":
-            subscription_task.cancel()
             await quit_game(stub)
             print("Bye, hope to see you soon!")
             break
         else:
-            print("\nSorry, incorrect command :(\nPlease try again\nAvailable commands: QUIT / PLAYERS" +
-                  ("/ SUBSCRIBE" if not is_subscribed else "") + "\n")
-
-        if subscription_task:
-            await asyncio.sleep(0.5)
+            print("\nSorry, incorrect command :(\nPlease try again\nAvailable commands: QUIT / REFRESH / PLAYERS\n")
+    task.cancel()
 
 
 async def run():
@@ -66,10 +58,8 @@ async def run():
     stub = server_pb2_grpc.MafiaServiceStub(channel)
 
     await set_username(stub)
-    print("Commands PLAYERS, SUBSCRIBE, and QUIT are now available to enter at any time!\n")
-
-    tasks = [commands_loop(stub), subscribe_to_notifications(stub)]
-    await asyncio.gather(*tasks)
+    print("Commands PLAYERS, REFRESH and QUIT are now available to enter at any time!\n")
+    await asyncio.create_task(commands_loop(stub))
 
 
 if __name__ == '__main__':
